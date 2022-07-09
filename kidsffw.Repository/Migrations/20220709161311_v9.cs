e1fba6169456bfc505ef435033d56ef9b1bb6e87
chanakya <chanakya.net@gmail.com>
2022-07-09 21:44:32 +0530

    fixed db update

---
+++ kidsffw.Repository/Migrations/20220709161311_v9.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace kidsffw.Repository.Migrations
+{
+    public partial class v9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRegistrationEntities",
+                table: "UserRegistrationEntities");
+
+            migrationBuilder.RenameTable(
+                name: "UserRegistrationEntities",
+                newName: "UserRegistrations");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRegistrations",
+                table: "UserRegistrations",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRegistrations",
+                table: "UserRegistrations");
+
+            migrationBuilder.RenameTable(
+                name: "UserRegistrations",
+                newName: "UserRegistrationEntities");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRegistrationEntities",
+                table: "UserRegistrationEntities",
+                column: "Id");
+        }
+    }
+}
