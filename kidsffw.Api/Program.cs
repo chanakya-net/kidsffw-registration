bffef3585626672b2e3980fae372d8580eaf91f2
chanakya <chanakya.net@gmail.com>
2022-07-13 20:08:34 +0530

    added rate ip limit

--- kidsffw.Api/Program.cs
+++ kidsffw.Api/Program.cs
@@ -1,11 +1,23 @@
+using AspNetCoreRateLimit;
 using kidsffw.Application;
 using kidsffw.Common.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddMemoryCache();
+
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRateLimitPolicies"));
+
+builder.Services.AddInMemoryRateLimiting();
+
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+
 // Add services to the container.
 builder.Services.AddCors(p=>p.AddPolicy("crossdomain", p=>p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -13,7 +25,10 @@ var razorPayConfiguration = builder.Configuration.GetSection("RazorPayConfigurat
 builder.Services.AddSingleton(razorPayConfiguration);
 builder.Services.AddApplication();
 
+
+
 var app = builder.Build();
+
 
 if (app.Environment.IsDevelopment())
 {
@@ -21,6 +36,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseIpRateLimiting();
+
 app.UseCors("crossdomain");
 app.UseHttpsRedirection();
 
